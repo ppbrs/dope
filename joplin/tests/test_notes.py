ee2450b11b7fd514411e8d62e540f6acf223d136
ppbrs <meemkcfr@proton.me>
2023-11-25 19:07:16 -0800

    Add test_notes_symbols

--- joplin/tests/test_notes.py
+++ joplin/tests/test_notes.py
@@ -1,5 +1,6 @@
 """ The module tests the contents of all notes in the local database."""
 # Standard library imports
+import logging
 import os
 # Third party imports
 # Local application/library imports
@@ -31,3 +32,24 @@ def test_notes_being_edited(db_local_notes: dict[common.JId32, common.JNote], lo
             logger.error(f"Note `{title}` is being edited. It should be closed.")
             edited_notes.append(title)
     assert len(edited_notes) == 0, "Some notes are opened for external editing."
+
+
+def test_notes_symbols(db_local_notes: dict[common.JId32, common.JNote], logger: logging.Logger):
+    """
+    Check that restricted symbols are not used in note's body.
+    """
+    logger.info(f"{len(db_local_notes)=}")
+    symbols = [
+        # " ",  # No break space.
+        " ",  # Hair space, or very thin space.
+    ]
+    line_cnt = 0
+    for _, note in db_local_notes.items():
+        for line in note.body.split("\n"):
+            for symbol in symbols:
+                if symbol in line:
+                    logger.error(f"found {symbol} in {note.title}")
+                    line_cnt += 1
+                    break
+    if line_cnt:
+        logger.error(f"{line_cnt} lines have restricted symbols.")
