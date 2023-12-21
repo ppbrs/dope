9b1ac7d7262711b9181f7c0d849764dcbff46eaa
ppbrs <meemkcfr@proton.me>
2023-12-20 22:28:18 -0800

    Enforce mypy disallow_untyped_decorators

--- joplin/conftest.py
+++ joplin/conftest.py
@@ -10,7 +10,7 @@ from joplin.common import FPATH_LOCAL_DB, get_db_local_notebooks, get_db_local_n
     get_db_local_tags, get_db_local_used_resources, JId32, JNote, JNotebook, JResource, JTag
 
 
-@pytest.fixture
+@pytest.fixture  # type: ignore[misc] # Untyped decorator makes function untyped
 def logger() -> logging.Logger:
     """ Logger fixture.
     Note that the logger name is conftest.py when using this fixture, which may be misleading,
@@ -19,14 +19,14 @@ def logger() -> logging.Logger:
     return logging.getLogger(__name__)
 
 
-@pytest.fixture
+@pytest.fixture  # type: ignore[misc] # Untyped decorator makes function untyped
 def db_local_conn() -> sqlite3.Connection:
     """ Open the local database and provide a connection object. """
     with sqlite3.connect(database=f"file:{FPATH_LOCAL_DB}?mode=ro", uri=True) as db_conn:
         yield db_conn
 
 
-@pytest.fixture
+@pytest.fixture  # type: ignore[misc] # Untyped decorator makes function untyped
 def db_local_notes() -> dict[JId32, JNote]:
     """
     Returns a dictionary with notes' IDs as keys and JNote objects as values.
@@ -34,7 +34,7 @@ def db_local_notes() -> dict[JId32, JNote]:
     return get_db_local_notes()
 
 
-@pytest.fixture
+@pytest.fixture  # type: ignore[misc] # Untyped decorator makes function untyped
 def db_local_notebooks() -> dict[JId32, JNotebook]:
     """
     Returns a dictionary with notebooks's id32 as key and Notebook object as value.
@@ -42,7 +42,7 @@ def db_local_notebooks() -> dict[JId32, JNotebook]:
     return get_db_local_notebooks()
 
 
-@pytest.fixture
+@pytest.fixture  # type: ignore[misc] # Untyped decorator makes function untyped
 def db_local_used_resources() -> dict[JId32, JResource]:
     """
     Returns a dictionary with notebooks's id32 as key and Resource object as value.
@@ -50,7 +50,7 @@ def db_local_used_resources() -> dict[JId32, JResource]:
     return get_db_local_used_resources()
 
 
-@pytest.fixture
+@pytest.fixture  # type: ignore[misc] # Untyped decorator makes function untyped
 def db_local_tags() -> dict[JId32, JTag]:
     """
     Returns a dictionary with tags's id32 as key and Resource object as value.
