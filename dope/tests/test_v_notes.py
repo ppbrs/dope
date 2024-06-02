b3c3f6403c8621909939d3e0a90472bdf14eb67a
ppbrs <meemkcfr@proton.me>
2024-06-02 16:41:31 -0700

    Add test_v_notes_titles

--- dope/tests/test_v_notes.py
+++ dope/tests/test_v_notes.py
@@ -22,3 +22,15 @@ def test_v_notes_newline() -> None:
                 cnt_no_empty_line += 1
     if cnt_no_empty_line:
         _logger.warning("%d notes don't have an empty line in the end.", cnt_no_empty_line)
+
+
+def test_v_notes_titles() -> None:
+    """Check if there are inappropriate symbols in note titles."""
+    for v_note in VNote.collect_iter(exclude_trash=True):
+        title = v_note.note_path.stem
+        symbols = ["`", "[", "]"]
+        for symbol in symbols:
+            if symbol in title:
+                logging.warning(
+                    "%s: Symbol `%s` in `%s`.",
+                    v_note.vault_dir.stem, symbol, v_note.note_path.relative_to(v_note.vault_dir))
