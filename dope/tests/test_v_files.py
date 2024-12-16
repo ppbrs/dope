b0b72c55b1cbdedba3d980122f384b637901a3ff
ppbrs <meemkcfr@proton.me>
2024-12-15 21:04:13 -0800

    Check that inboxes exist and are empty

--- dope/tests/test_v_files.py
+++ dope/tests/test_v_files.py
@@ -32,3 +32,28 @@ def test_v_files_trash() -> None:
             trash_files.append(str(path_rel))
             logging.warning("%s: %s.", vault_dir.stem, path_rel)
     assert not trash_files, f"Found files in .trash directories: {trash_files}"
+
+
+def test_v_files_inbox() -> None:
+    """
+    Check inboxes.
+
+    _inbox directory should exist.
+    _inbox directory should contain only a .keep file.
+    """
+    inbox_files = []
+    for vault_dir in V_DIRS:
+        inbox_dir = vault_dir / "_inbox"
+        assert inbox_dir.exists(), f"'{inbox_dir.relative_to(vault_dir.parent)}' not found."
+        assert inbox_dir.is_dir(), \
+            f"'{inbox_dir.relative_to(vault_dir.parent)}' is not a directory."
+        inbox_keep = inbox_dir / ".keep"
+        assert inbox_keep.exists(), f"'{inbox_keep.relative_to(vault_dir.parent)}' not found."
+        assert inbox_keep.is_file(), \
+            f"'{inbox_keep.relative_to(vault_dir.parent)}' is not a file."
+        for path in vault_dir.rglob("_inbox/*"):
+            if path.name != ".keep":
+                path_rel = path.relative_to(vault_dir.parent)
+                inbox_files.append(str(path_rel))
+                logging.warning("%s: %s.", vault_dir.stem, path_rel)
+    assert not inbox_files, f"Found files in inboxes: {inbox_files}"
