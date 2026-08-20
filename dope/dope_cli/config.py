749796412749df1fca357c39701388fd2678f31c
Boris Prishchepenko <boris@zaber.com>
2026-08-20 16:03:08 -0700

    WIP clean up config

--- dope/dope_cli/config.py
+++ dope/dope_cli/config.py
@@ -3,44 +3,28 @@ Processing user requests in the dope command-line tool
 that are related to dope configuration
 """
 
-from pathlib import PosixPath
+import os
+import subprocess
 from typing import Any
 
-from dope.config import add_vault, drop_vault, get_vault_paths
+from dope.config import get_config_path
 
 
-def process_arguments(args: dict[str, Any]) -> int:
-    """Process arguments related to vaults configuration."""
-    assert "config_vault_add" in args
-    assert "config_vault_list" in args
-    assert "config_vault_drop" in args
+def process_config_arguments(args: dict[str, Any]) -> int:
+    """Process arguments related to configuration."""
+    assert "config_editor" in args
+    config_editor = args["config_editor"]
+    if config_editor is not None:
+        assert isinstance(config_editor, list)
+        assert len(config_editor) == 1
+        assert isinstance(config_editor[0], str)
 
-    if (paths_add := args["config_vault_add"]) and paths_add:
-        assert isinstance(paths_add, list) and all(isinstance(path, str) for path in paths_add)
-        for path in paths_add:
-            vault_path = PosixPath(path).expanduser()
-            assert vault_path.exists() and vault_path.is_dir()
-            if add_vault(vault_path=vault_path):
-                print(f"\nAdded '{vault_path}' to the configuration.")
-            else:
-                print(f"\nVault '{vault_path}' in already in the configuration.")
-
-    if args["config_vault_list"]:
-        if vault_paths := get_vault_paths():
-            print("\nConfigured vaults:")
-            for vault in vault_paths:
-                print(f"\t{vault}")
-        else:
-            print("No configured vaults.")
-
-    if (paths_drop := args["config_vault_drop"]) and paths_drop:
-        assert isinstance(paths_drop, list) and all(isinstance(path, str) for path in paths_drop)
-        for path in paths_drop:
-            vault_path = PosixPath(path).expanduser()
-            assert vault_path.exists() and vault_path.is_dir()
-            if drop_vault(vault_path=vault_path):
-                print(f"\nRemoved '{vault_path}' from the configuration.")
-            else:
-                print(f"\nVault '{vault_path}' in not in the configuration.")
+        process = subprocess.Popen(
+            [config_editor[0], get_config_path()],
+            stdout=open(os.devnull, "wb"),
+            stderr=open(os.devnull, "wb"),
+            stdin=subprocess.DEVNULL,
+            start_new_session=True,
+        )
 
     return 0
